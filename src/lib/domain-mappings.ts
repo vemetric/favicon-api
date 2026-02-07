e958d597d4d4927f0bbb7d7fc5b27878c8d6b9b8
Dominik Sumer <domi.sumer@gmail.com>
2026-02-07 15:24:33 +0100

    added snapchat mapping

--- src/lib/domain-mappings.ts
+++ src/lib/domain-mappings.ts
@@ -7,6 +7,7 @@ const DOMAIN_MAPPINGS: Record<string, string> = {
   'com.google.android.googlequicksearchbox': 'google.com',
   'com.google.android.gm': 'gmail.com',
   'com.pinterest': 'pinterest.com',
+  snapchat: 'snapchat.com',
 };
 
 /**
